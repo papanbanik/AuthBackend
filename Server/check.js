7d3930b2c7372b80fc8a6503ee83fe2864b48289
Papan Banik <papanbanik58@gmail.com>
2026-05-31 12:13:16 +0600

    first-commit

--- Server/check.js
+++
@@ -1,7 +0,0 @@
-import https from "https";
-
-https.get("https://api.ipify.org?format=json", res => {
-  let data = "";
-  res.on("data", chunk => data += chunk);
-  res.on("end", () => console.log("Server IP:", data));
-});
