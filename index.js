2e8c742a5a10f0a9c9ad624cbd15ac41b08b47b7
Papan Banik <Papan.Banik@siarasolution.com>
2026-06-13 01:51:00 +0600

    update-form data

--- index.js
+++ index.js
@@ -7,15 +7,30 @@ import route from './router.js'
 import connectDB from './config/mongodb.js'
 import './config/passport.js'
 const app = express()
+const whitelist = [
+  'http://localhost:3000',
+  'https://auth-frontend-9vkd.vercel.app',
+  process.env.CLIENT_URL,
+  process.env.PRODUCTION_URL
+].filter(Boolean)
+
 app.use(cors({
-  origin: [
-    'http://localhost:3000',
-    'https://auth-frontend-9vkd.vercel.app'
-  ],
-  methods: ["GET", "POST", "PUT", "DELETE"],
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true)
+    if (whitelist.indexOf(origin) !== -1) {
+      return callback(null, true)
+    }
+    return callback(new Error('Not allowed by CORS'))
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   credentials: true,
-  allowedHeaders: ["Content-Type", "Authorization"]
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept', 'Origin'],
+  preflightContinue: false,
+  optionsSuccessStatus: 204
 }))
+
+// Ensure OPTIONS preflight requests are handled for all routes
+app.options('*', cors())
 
 app.use(express.json())
 app.use(cookieParser())
